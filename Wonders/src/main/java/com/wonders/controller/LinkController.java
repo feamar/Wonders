ee33f8fadfbf3525bef492d53a9f822c09aa1dc8
Teije van Sloten <teije-vs@hotmail.com>
2015-03-08 01:46:52 +0100

    Initial setup
    Gitignore added, Webapplication with Maven added, Jquery and bootstrap
    added, simple controller added

---
+++ Wonders/src/main/java/com/wonders/controller/LinkController.java
@@ -0,0 +1,36 @@
+package com.wonders.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "LinkController", urlPatterns = {"/home"})
+public class LinkController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        request.getRequestDispatcher("/WEB-INF/view/"+ request.getServletPath() +".jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
